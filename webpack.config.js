e059f65c3b3e61152ebb278a01a656d0c284a978
TalIq501 <talhaiqbal501@gmail.com>
2025-02-05 13:51:43 +0500

    Changed to template.html as per requirement

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     devtool: "eval-source-map",
     devServer: {
-        watchFiles: ["./src/index.html"],
+        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
